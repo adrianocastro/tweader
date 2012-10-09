c8c8d8b3023ea3aff6fbf85bd178fb79e03fb469
Adriano Castro <ad@adrianocastro.net>
2012-10-09 09:59:26 -0700

    Added controls to remove topics.

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,61 @@
-// // @TODO: namespace
-// $(function(){
+// @TODO: namespace
+$(function(){
 
-//     // This will talk to socket.io
-//     var socket = io.connect('http://localhost:3000');
+    // // This will talk to socket.io
+    // var socket = io.connect('http://localhost:3000');
 
-//     socket.on('feed-update', function (data) {
-//         // Pass data to function to render new items into the feed
-//     });
-//     socket.on('newtweet', function (data) {
-//         console.log('we got new tweets', data);
-//     });
+    // socket.on('feed-update', function (data) {
+    //     // Pass data to function to render new items into the feed
+    // });
+    // socket.on('newtweet', function (data) {
+    //     console.log('we got new tweets', data);
+    // });
 
-//     // Handle topic search form interactions
-//     $('#topic-search').submit(function(e) {
-//         // Prevent the form submit
-//         e.preventDefault();
-//         // Temporarily disable the submit button until the ajax call is complete
-//         var submitButton = $('input[type=submit]', this);
-//         submitButton.attr('disabled', 'disabled');
+    // // Handle topic search form interactions
+    // $('#topic-search').submit(function(e) {
+    //     // Prevent the form submit
+    //     e.preventDefault();
+    //     // Temporarily disable the submit button until the ajax call is complete
+    //     var submitButton = $('input[type=submit]', this);
+    //     submitButton.attr('disabled', 'disabled');
 
-//         $.ajax({
-//             url: "/.json",
-//             data: $(this).serialize(),
-//             success: function(data) {
-//                 console.log('data', data);
-//             }
-//         }).done(function() {
-//             // Re-enable the submit button
-//             submitButton.removeAttr('disabled');
-//         });
-//     });
+    //     $.ajax({
+    //         url: "/.json",
+    //         data: $(this).serialize(),
+    //         success: function(data) {
+    //             console.log('data', data);
+    //         }
+    //     }).done(function() {
+    //         // Re-enable the submit button
+    //         submitButton.removeAttr('disabled');
+    //     });
+    // });
 
-//     function addToFeed(tweets) {
-//         // add new tweets to existing feed
-//     }
+    $("#topic-list").on("click", ".topic-remove", function(e){
+        e.preventDefault();
 
-//     function addToTopicList(topics) {
-//         // add new topics to topic list
-//     }
+        var topic       = $(this).parent(),
+            topicId     = topic.attr('data-id'),
+            topicTweets = $('#feed').find("[data-id='" + topicId + "']"),
+            savedTopics = $('#savedTerms');
 
-//     function removeFromTopicList(topics) {
-//         // remove topics from topic list
-//     }
-// });
+        // remove all tweets for topic from the feed
+        topicTweets.remove();
+        // remove topic itself from topic list
+        topic.remove();
+        // and remove topic from list of saved topics
+        savedTopics.val(savedTopics.val().replace(topicId + ',', ''));
+    });
+
+    // function addToFeed(tweets) {
+    //     // add new tweets to existing feed
+    // }
+
+    // function addToTopicList(topics) {
+    //     // add new topics to topic list
+    // }
+
+    // function removeFromTopicList(topics) {
+    //     // remove topics from topic list
+    // }
+});
